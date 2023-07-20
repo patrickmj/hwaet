76eeeacbdb767673d0fd1802f6e443b0aa988a21
patrickmj <p.murray-john@northeastern.edu>
2023-07-20 17:28:40 -0400

    complete rewrite to simplify data
    splits Hwaet class from Term class
    just the core structure

--- data/terms.js
+++
@@ -1,1 +0,0 @@
-[{"id":0,"term":"hwaet","sameAs":[]},{"id":1,"term":"shit2","sameAs":[]},{"id":2,"term":"shit","sameAs":[]},{"id":3,"term":"shit3","sameAs":[]}]
--- hwaet.php
+++ hwaet.php
@@ -7,86 +7,85 @@ error_reporting(E_ALL);
 if (isset($_GET['term'])) {
     $term = $_GET['term'];
     $ipAddress = $_SERVER['REMOTE_ADDR'];
+    if($ipAddress == '127.0.0.1') {
+        $ipAddress = rand(1, 10000 );
+    }
+    echo $ipAddress;
     $hwaet = new Hwaet($term, $ipAddress);
 }
 
 class Hwaet {
-    private string $termsFileName = 'data/terms.js';
-    private string $ipAddressVotesFileName = 'data/ipAddressVotes.js';
-    private string $votesFileName = 'data/votes.js';
-    protected array $terms = [];
-    protected array $ipAddressVotes = [];
-    protected array $votes = [];
-    protected string $ipAddress;
+    protected string $termsFilePath = 'data/terms.json';
+    protected array $terms;
 
     public function __construct(string $term, string $ipAddress) {
-        $this->init($ipAddress);
-        $this->upvoteTerm($term);
+        $this->init($term, $ipAddress);
     }
 
-    public function init(string $ipAddress) {
-        $this->terms = json_decode(file_get_contents($this->termsFileName), true);
-        $this->ipAddressVotes = json_decode(file_get_contents($this->ipAddressVotesFileName), true);
-        $this->votes = json_decode(file_get_contents($this->votesFileName), true);
-        $this->ipAddress = $ipAddress;
-    }
-
-    public function addIpAddressVote(string $ipAddress, int $termId) {
-        $this->ipAddressVotes[$ipAddress][] = $termId;
-        print_r($this->ipAddressVotes[$ipAddress]);
-    }
-
-    public function checkIpAddressVote(string $ipAddress, int $termId) {
-        print_r($this->ipAddressVotes);
-        echo $ipAddress;
-        //die();
-        if(array_key_exists($ipAddress, $this->ipAddressVotes)) {
-            echo "cheeky devil!";
-        } else {
-            echo "ok";
-            $this->addIpAddressVote($ipAddress, $termId);
+    protected function init(string $term, string $ipAddress) {
+        $termsArray = json_decode(file_get_contents($this->termsFilePath), true);
+        foreach($termsArray as $term=>$termData) {
+            $term = new Term($term, $termData);
+            $this->terms[] = $term;
         }
     }
 
-    /**
-     * adds a term if it doesn't already exist
-     * otherwise, returns its ID after checking if it's a cheeky devil's IP address
-     */
-    public function upvoteTerm(string $upvoteTerm) {
-        $termExists = false;
-        $termId = null;
-        foreach ($this->terms as $term => $termData) {
-            if ($termData['term'] == $upvoteTerm) {
-                $termExists = true;
-                $termId = $termData['id']; 
-                break;
-            }
-        }
-        
-        if ($termExists) {
-            //vscode thinks this is still null b/c it doesn't know the
-            //logic of when it gets set above,
-            //hence the check for whether it's null to make it STFU
-            if (! is_null($termId)) {
-                $this->checkIpAddressVote($this->ipAddress, $termId);
-                $this->addVote($termId);
-            }
-        } else {
-            $termsLength = count($this->terms);
-            $this->terms[] = ['id' => $termsLength,
-                              'term' => $upvoteTerm,
-                              'sameAs' => []
-                             ];
-                             
-            //put a lock on the file during the transaction?
-            //need to test with many people upvoting at same time
-            //in effect, attempt a DOS attack
-            file_put_contents($this->termsFileName, json_encode($this->terms));
-        }
+    protected function createTerm(string $term, string $ipAddress) {
+        $termData = [
+            "votes" => 1,
+            "cheekyIps" => [],
+            //not worrying about whether there's a sameAs here,
+            //since that's something for moderators to monitor and
+            //update manually elsewhere
+            "sameAs" => []
+        ];
+
+        $newTerm = new Term($term, $termData);
+        $this->terms[$term] = $newTerm;
     }
 
-    public function addVote(int $termId) {
-       $this->votes[$termId]++;
-       file_put_contents($this->votesFileName, json_encode($this->votes));
+    //would be different in a SQL world
+    protected function updateTerms(): void {
+        $toUpdateArray = []; 
+        foreach($this->terms as $term=>$termObject) {
+            $toUpdateArray[$term] = $termObject->toArray();
+        }
+        file_put_contents($this->termsFilePath, $toUpdateArray);
+    }
+}
+
+class Term {
+    protected string $term;
+    protected int $votes;
+    protected array $cheekyIps;
+    protected array $sameAs;
+
+    public function __construct(string $term, array $termData ) {
+        $this->term = $term;
+        $this->votes = $termData['votes'];
+        $this->cheekyIps = $termData['cheekyIps'];
+        $this->sameAs = $termData['sameAs'];
+    }
+
+    public function updateCheekyIps(string $ipAddress): void {
+        $this->cheekyIps[] = $ipAddress;
+    }
+
+    public function updateSameAs(string $sameAsTerm): void {
+        $this->sameAs[] = $sameAsTerm;
+    }
+
+    public function incrementVotes() {
+        $this->votes++;
+    }
+
+    public function toArray(): array {
+        $dataArray = [
+            "votes" => $this->votes,
+            "cheekyIps" => $this->cheekyIps,
+            "sameAs" => $this->sameAs,
+        ];
+
+        return $dataArray;
     }
 }
